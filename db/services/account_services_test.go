50cbb971dac5a16c2f31b0110989eaa15e9aa0af
thefeij <abolfazl.moradi.feijani@gmail.com>
2023-12-18 22:40:26 +0330

    Added a unit test for GetAccount service

--- db/services/account_services_test.go
+++ db/services/account_services_test.go
@@ -1,13 +1,14 @@
 package services
 
 import (
+	"Simple-Bank/db/models"
 	"Simple-Bank/requests"
 	"Simple-Bank/util"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) models.Accounts {
 	testAccount := requests.CreateAccountRequest{
 		Owner:    util.RandomOwner(),
 		Currency: util.RandomCurrency(),
@@ -22,4 +23,26 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	response, err := accountServices.getAccount(uint64(account.ID))
+
+	require.NoError(t, err)
+	require.NotEmpty(t, response)
+
+	require.Equal(t, account.ID, response.AccountID)
+	require.Equal(t, account.Currency, response.Currency)
+	require.Equal(t, account.Balance, response.Balance)
+	require.Equal(t, account.CreatedAt, response.CreatedAt)
+	require.Equal(t, account.Owner, response.Owner)
+	//require.WithinDuration(t, account.CreatedAt, response.CreatedAt, time.Second)
 }
