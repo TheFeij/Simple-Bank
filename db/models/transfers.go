33adb7cc7f4f37318928605acb448a695cdcfbde
thefeij <abolfazl.moradi.feijani@gmail.com>
2023-12-28 12:57:48 +0330

    Added IncomingEntryID and OutGoingEntryID to Transfers struct and Incoming and outGoing Entries

--- db/models/transfers.go
+++ db/models/transfers.go
@@ -6,7 +6,11 @@ import (
 
 type Transfers struct {
 	gorm.Model
-	FromAccountID uint64 `gorm:"type:bigint;not null"`
-	ToAccountID   uint64 `gorm:"type:bigint;not null"`
-	Amount        uint32 `gorm:"type:bigint;not null"`
+	FromAccountID   uint64  `gorm:"type:bigint;not null"`
+	ToAccountID     uint64  `gorm:"type:bigint;not null"`
+	Amount          uint32  `gorm:"type:bigint;not null"`
+	IncomingEntryID uint64  `gorm:"not null"`
+	OutGoingEntryID uint64  `gorm:"not null"`
+	IncomingEntry   Entries `gorm:"foreignKey:AccountID;references:IncomingEntryID"`
+	OutgoingEntry   Entries `gorm:"foreignKey:AccountID;references:OutGoingEntryID"`
 }
