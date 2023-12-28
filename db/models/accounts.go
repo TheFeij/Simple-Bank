f18efb4cef39799658ab2b9915566a18cd6f1a1e
thefeij <abolfazl.moradi.feijani@gmail.com>
2023-12-28 19:30:36 +0330

    changed db models structs to not using GORM associations

--- db/models/accounts.go
+++ db/models/accounts.go
@@ -6,9 +6,6 @@ import (
 
 type Accounts struct {
 	gorm.Model
-	Owner    string      `gorm:"type:varchar(50);not null"`
-	Balance  uint64      `gorm:"type:bigInt;default:0;not null"`
-	Entries  []Entries   `gorm:"foreignKey:AccountID;references:ID"`
-	Incoming []Transfers `gorm:"foreignKey:ToAccountID;references:ID"`
-	Outgoing []Transfers `gorm:"foreignKey:FromAccountID;references:ID"`
+	Owner   string `gorm:"type:varchar(50);not null"`
+	Balance uint64 `gorm:"type:bigInt;default:0;not null"`
 }
