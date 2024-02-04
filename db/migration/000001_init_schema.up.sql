dd40c71a040cd000b61d2e89972504777c0f5595
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-04 18:41:49 +0330

    made references to accounts.id be on delete cascade

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,21 +1,21 @@
 create table accounts(
-                         id bigserial primary key,
-                         balance bigint default 0 not null,
-                         owner varchar(50) not null,
-                         createdAt timestamptz default now() not null,
+    id bigserial primary key,
+    balance bigint default 0 not null,
+    owner varchar(50) not null,
+    createdAt timestamptz default now() not null
 );
 
 create table entries(
-                        id bigserial primary key,
-                        account_id bigint references accounts(id) not null,
-                        amount bigint not null,
-                        createdAt timestamptz default now() not null
+    id bigserial primary key,
+    account_id bigint references accounts(id) on delete cascade not null,
+    amount bigint not null,
+    createdAt timestamptz default now() not null
 );
 
 create table transfers(
-                          id bigserial primary key,
-                          from_account_id bigint references accounts(id) not null,
-                          to_account_id bigint references accounts(id) not null,
-                          amount bigint not null,
-                          createdAt timestamptz default now() not null
+    id bigserial primary key,
+    from_account_id bigint references accounts(id) on delete cascade not null ,
+    to_account_id bigint references accounts(id) on delete cascade not null,
+    amount bigint not null,
+    createdAt timestamptz default now() not null
 )
