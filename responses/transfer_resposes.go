07e5c3bf875c55aa3a274b993468b9abd6370f47
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-04 22:20:54 +0330

    Added Incoming/OutgoingEntryID fields to TransferResponse struct

--- responses/transfer_resposes.go
+++ responses/transfer_resposes.go
@@ -3,9 +3,11 @@ package responses
 import "time"
 
 type TransferResponse struct {
-	TransferID   uint64    `json:"transfer_id"`
-	SrcAccountID uint64    `json:"src_account_id"`
-	DstAccountID uint64    `json:"dst_account_id"`
-	CreatedAt    time.Time `json:"created_at"`
-	Amount       int64     `json:"amount"`
+	TransferID      uint64    `json:"transfer_id"`
+	SrcAccountID    uint64    `json:"src_account_id"`
+	DstAccountID    uint64    `json:"dst_account_id"`
+	IncomingEntryID uint64    `json:"incoming_entry_id"`
+	OutgoingEntryID uint64    `json:"out_going_entry_id"`
+	CreatedAt       time.Time `json:"created_at"`
+	Amount          int64     `json:"amount"`
 }
