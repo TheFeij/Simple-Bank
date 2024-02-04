3c2d88e9f768d229ec97407469c23d89f0f38478
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-04 19:14:16 +0330

    renamed Time field in Transfer response struct to CreatedAt

--- responses/transfer_resposes.go
+++ responses/transfer_resposes.go
@@ -6,6 +6,6 @@ type TransferResponse struct {
 	TransferID   uint64    `json:"transfer_id"`
 	SrcAccountID uint64    `json:"src_account_id"`
 	DstAccountID uint64    `json:"dst_account_id"`
-	Time         time.Time `json:"time"`
+	CreatedAt    time.Time `json:"created_at"`
 	Amount       int64     `json:"amount"`
 }
