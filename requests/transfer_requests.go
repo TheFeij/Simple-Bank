fbf6a17198bc9174c369fd15b7a1dfe30531efd3
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-05 21:34:46 +0330

    changed validate to binding for input validation

--- requests/transfer_requests.go
+++ requests/transfer_requests.go
@@ -1,7 +1,7 @@
 package requests
 
 type TransferRequest struct {
-	FromAccountID uint64 `json:"from_account_id" validate:"required;nefield=ToAccountID"`
-	ToAccountID   uint64 `json:"to_account_id" validate:"required"`
-	Amount        uint32 `json:"amount" validate:"required;gt=0"`
+	FromAccountID uint64 `json:"from_account_id" binding:"required;nefield=ToAccountID"`
+	ToAccountID   uint64 `json:"to_account_id" binding:"required"`
+	Amount        uint32 `json:"amount" binding:"required;gt=0"`
 }
--- requests/user_requests.go
+++ requests/user_requests.go
@@ -1,16 +1,16 @@
 package requests
 
 type CreateAccountRequest struct {
-	Owner   string `json:"owner" validate:"required;max=50;min=2"`
-	Balance uint64 `json:"balance" validate:"gt=0"`
+	Owner   string `json:"owner" binding:"required;max=50;min=2"`
+	Balance uint64 `json:"balance" binding:"gt=0"`
 }
 
 type DepositRequest struct {
-	AccountID uint64 `json:"account_id" validate:"required"`
-	Amount    uint32 `json:"amount" validate:"required;gt=0"`
+	AccountID uint64 `json:"account_id" binding:"required"`
+	Amount    uint32 `json:"amount" binding:"required;gt=0"`
 }
 
 type WithdrawRequest struct {
-	AccountID uint64 `json:"account_id" validate:"required"`
-	Amount    uint32 `json:"amount" validate:"required;gt=0"`
+	AccountID uint64 `json:"account_id" binding:"required"`
+	Amount    uint32 `json:"amount" binding:"required;gt=0"`
 }
