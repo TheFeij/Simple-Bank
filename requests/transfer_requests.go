6d63cf4e52dbcc250cbd46e038010df5637692ef
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-06 11:54:20 +0330

    fixed an error, tags in the binding clause must be separated with comma

--- requests/transfer_requests.go
+++ requests/transfer_requests.go
@@ -1,7 +1,7 @@
 package requests
 
 type TransferRequest struct {
-	FromAccountID uint64 `json:"from_account_id" binding:"required;nefield=ToAccountID"`
+	FromAccountID uint64 `json:"from_account_id" binding:"required,nefield=ToAccountID"`
 	ToAccountID   uint64 `json:"to_account_id" binding:"required"`
-	Amount        uint32 `json:"amount" binding:"required;gt=0"`
+	Amount        uint32 `json:"amount" binding:"required,gt=0"`
 }
--- requests/user_requests.go
+++ requests/user_requests.go
@@ -1,16 +1,16 @@
 package requests
 
 type CreateAccountRequest struct {
-	Owner   string `json:"owner" binding:"required;max=50;min=2"`
+	Owner   string `json:"owner" binding:"required,alpha"`
 	Balance uint64 `json:"balance" binding:"gt=0"`
 }
 
 type DepositRequest struct {
 	AccountID uint64 `json:"account_id" binding:"required"`
-	Amount    uint32 `json:"amount" binding:"required;gt=0"`
+	Amount    uint32 `json:"amount" binding:"required,gt=0"`
 }
 
 type WithdrawRequest struct {
 	AccountID uint64 `json:"account_id" binding:"required"`
-	Amount    uint32 `json:"amount" binding:"required;gt=0"`
+	Amount    uint32 `json:"amount" binding:"required,gt=0"`
 }
