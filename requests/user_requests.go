4c0123bacee154a3b468690f13b4da026452c1c9
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-06 12:25:39 +0330

    Defined GetAccountsListRequest struct

--- requests/user_requests.go
+++ requests/user_requests.go
@@ -9,6 +9,11 @@ type GetAccountRequest struct {
 	ID uint64 `uri:"id" binding:"required,min=1"`
 }
 
+type GetAccountsListRequest struct {
+	PageID   uint64 `form:"page_id" binding:"required,min=1"`
+	PageSize uint64 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
 type DepositRequest struct {
 	AccountID uint64 `json:"account_id" binding:"required"`
 	Amount    uint32 `json:"amount" binding:"required,gt=0"`
