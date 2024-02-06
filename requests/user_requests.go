25be82d4ccce053a6c1cd4d62aec5f322079f081
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-06 12:23:49 +0330

    Defined GetAccountRequest struct

--- requests/user_requests.go
+++ requests/user_requests.go
@@ -5,6 +5,10 @@ type CreateAccountRequest struct {
 	Balance uint64 `json:"balance" binding:"gte=0"`
 }
 
+type GetAccountRequest struct {
+	ID uint64 `uri:"id" binding:"required,min=1"`
+}
+
 type DepositRequest struct {
 	AccountID uint64 `json:"account_id" binding:"required"`
 	Amount    uint32 `json:"amount" binding:"required,gt=0"`
