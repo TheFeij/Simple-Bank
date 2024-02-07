d57ef2c4c23586ad826afec6dc0dec4418b7c391
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-07 21:46:53 +0330

    all id fields are changed to be int64, and amount to be int32

--- responses/transfer_resposes.go
+++ responses/transfer_resposes.go
@@ -3,11 +3,11 @@ package responses
 import "time"
 
 type TransferResponse struct {
-	TransferID      uint64    `json:"transfer_id"`
-	SrcAccountID    uint64    `json:"src_account_id"`
-	DstAccountID    uint64    `json:"dst_account_id"`
-	IncomingEntryID uint64    `json:"incoming_entry_id"`
-	OutgoingEntryID uint64    `json:"out_going_entry_id"`
+	TransferID      int64     `json:"transfer_id"`
+	SrcAccountID    int64     `json:"src_account_id"`
+	DstAccountID    int64     `json:"dst_account_id"`
+	IncomingEntryID int64     `json:"incoming_entry_id"`
+	OutgoingEntryID int64     `json:"out_going_entry_id"`
 	CreatedAt       time.Time `json:"created_at"`
-	Amount          uint32    `json:"amount"`
+	Amount          int32     `json:"amount"`
 }
