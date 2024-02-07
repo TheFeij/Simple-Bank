84896778a48008737a43ad73891fed6abb61ee6e
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-07 21:44:15 +0330

    all id fields are changed to be int64, and amount to be int32

--- requests/transfer_requests.go
+++ requests/transfer_requests.go
@@ -1,7 +1,7 @@
 package requests
 
 type TransferRequest struct {
-	FromAccountID uint64 `json:"from_account_id" binding:"required,min=1,nefield=ToAccountID"`
-	ToAccountID   uint64 `json:"to_account_id" binding:"required,min=1"`
-	Amount        uint32 `json:"amount" binding:"required,gt=0"`
+	FromAccountID int64 `json:"from_account_id" binding:"required,min=1,nefield=ToAccountID"`
+	ToAccountID   int64 `json:"to_account_id" binding:"required,min=1"`
+	Amount        int32 `json:"amount" binding:"required,gt=0"`
 }
