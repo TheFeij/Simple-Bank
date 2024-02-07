6a5dbae54dc2a416d0ea1b9eeba17e6507e34954
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-07 13:01:20 +0330

    added min validator to ID fields

--- requests/transfer_requests.go
+++ requests/transfer_requests.go
@@ -1,7 +1,7 @@
 package requests
 
 type TransferRequest struct {
-	FromAccountID uint64 `json:"from_account_id" binding:"required,nefield=ToAccountID"`
-	ToAccountID   uint64 `json:"to_account_id" binding:"required"`
+	FromAccountID uint64 `json:"from_account_id" binding:"required,min=1,nefield=ToAccountID"`
+	ToAccountID   uint64 `json:"to_account_id" binding:"required,min=1"`
 	Amount        uint32 `json:"amount" binding:"required,gt=0"`
 }
