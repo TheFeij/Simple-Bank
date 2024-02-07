621664fbb88a3b3bf04894bc3a3388e32e9cc10c
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-07 21:30:56 +0330

    redefined model, removed gorm.model from model to directly have access to all the fields of model and to also be able to change field types

--- db/models/accounts.go
+++ db/models/accounts.go
@@ -2,10 +2,14 @@ package models
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type Accounts struct {
-	gorm.Model
-	Owner   string `gorm:"column:owner"`
-	Balance uint64 `gorm:"column:balance"`
+	ID        int64          `gorm:"column:id"`
+	Owner     string         `gorm:"column:owner"`
+	Balance   int64          `gorm:"column:balance"`
+	CreatedAt time.Time      `gorm:"column:created_at"`
+	UpdatedAt time.Time      `gorm:"column:updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at"`
 }
