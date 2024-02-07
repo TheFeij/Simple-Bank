eee6787351a89d41ebd75b6e0d19e36d2422afbe
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-07 18:12:57 +0330

    Created a ValidPassword custom validator

--- requests/validators.go
+++ requests/validators.go
@@ -16,3 +16,30 @@ var ValidUsername validator.Func = func(fl validator.FieldLevel) bool {
 	}
 	return false
 }
+
+var ValidPassword validator.Func = func(fl validator.FieldLevel) bool {
+	if password, ok := fl.Field().Interface().(string); ok {
+		if len(password) < 8 || len(password) > 64 {
+			return false
+		}
+
+		if match, _ := regexp.MatchString("^[a-zA-Z0-9_!@#$%&*^]*$", password); !match {
+			return false
+		}
+		if match, _ := regexp.MatchString("^.*[a-z].*$", password); !match {
+			return false
+		}
+		if match, _ := regexp.MatchString("^.*[A-Z].*$", password); !match {
+			return false
+		}
+		if match, _ := regexp.MatchString("^.*[0-9].*$", password); !match {
+			return false
+		}
+		if match, _ := regexp.MatchString("^.*[_!@#$%&*^].*$", password); !match {
+			return false
+		}
+
+		return true
+	}
+	return false
+}
