f46cf5773c23a58d1fc40b08b0eafbba3bf3aa59
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-07 21:35:10 +0330

    redefined model, removed gorm.model from model to directly have access to all the fields of model and to also be able to change field types

--- db/models/transfers.go
+++ db/models/transfers.go
@@ -2,13 +2,18 @@ package models
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type Transfers struct {
-	gorm.Model
-	FromAccountID   uint64 `gorm:"column:from_account_id"`
-	ToAccountID     uint64 `gorm:"column:to_account_id"`
-	Amount          uint32 `gorm:"column:amount"`
-	IncomingEntryID uint64 `gorm:"column:incoming_entry_id"`
-	OutgoingEntryID uint64 `gorm:"column:outgoing_entry_id"`
+	ID            int64 `gorm:"column:id"`
+	FromAccountID int64 `gorm:"column:from_account_id"`
+	ToAccountID   int64 `gorm:"column:to_account_id"`
+	// Amount must be positive here so the range is [1, maxint32]
+	Amount          int32          `gorm:"column:amount"`
+	IncomingEntryID int64          `gorm:"column:incoming_entry_id"`
+	OutgoingEntryID int64          `gorm:"column:outgoing_entry_id"`
+	CreatedAt       time.Time      `gorm:"column:created_at"`
+	UpdatedAt       time.Time      `gorm:"column:updated_at"`
+	DeletedAt       gorm.DeletedAt `gorm:"column:deleted_at"`
 }
