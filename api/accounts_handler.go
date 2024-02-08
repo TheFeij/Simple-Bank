3d41851758dd0730472d24da72b8f4a1c77c0ab8
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-08 15:15:56 +0330

    Created GetUser handler

--- api/accounts_handler.go
+++ api/accounts_handler.go
@@ -81,3 +81,23 @@ func (handler *Handler) CreateUser(context *gin.Context) {
 
 	context.JSON(http.StatusOK, res)
 }
+
+func (handler *Handler) GetUser(context *gin.Context) {
+	var req requests.GetUserRequest
+	if err := context.ShouldBindUri(&req); err != nil {
+		context.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	res, err := handler.services.GetUser(req.Username)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			context.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		context.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	context.JSON(http.StatusOK, res)
+}
