5bfee17df970ae4cb0013543708cd5ba70b2eeb0
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-08 18:21:02 +0330

    created a Test for HashPassword and CheckPassword function

---
+++ util/password_test.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+	"testing"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomPassword()
+
+	hashedPassword, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+
+	err = CheckPassword(password, hashedPassword)
+	require.NoError(t, err)
+
+	wrongPassword := RandomPassword()
+	err = CheckPassword(wrongPassword, hashedPassword)
+	require.Error(t, err)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	hashedPassword2, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword2)
+
+	require.NotEqual(t, hashedPassword, hashedPassword2)
+}
