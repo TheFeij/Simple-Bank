307501c89f503e06de949fdb448d7b43e5ad6290
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-08 15:07:54 +0330

    set timezone to UTC, also removed not null constraint on timestampz fields

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -2,18 +2,18 @@ create table accounts(
     id bigserial primary key,
     balance bigint default 0 not null,
     owner varchar(64) not null,
-    created_at timestamptz default now() not null,
-    updated_at timestamptz default now() not null,
-    deleted_at timestamptz default '0001-01-01 00:00:00Z' not null
+    created_at timestamptz default now(),
+    updated_at timestamptz default now(),
+    deleted_at timestamptz default '0001-01-01 00:00:00Z'
 );
 
 create table entries(
     id bigserial primary key,
     account_id bigint references accounts(id) on delete cascade not null,
     amount int not null,
-    created_at timestamptz default now() not null,
-    updated_at timestamptz default now() not null,
-    deleted_at timestamptz default '0001-01-01 00:00:00Z' not null
+    created_at timestamptz default now(),
+    updated_at timestamptz default now(),
+    deleted_at timestamptz default '0001-01-01 00:00:00Z'
 );
 
 create table transfers(
@@ -23,7 +23,9 @@ create table transfers(
     incoming_entry_id bigint references entries(id) on delete cascade not null,
     outgoing_entry_id bigint references entries(id) on delete cascade not null,
     amount int not null,
-    created_at timestamptz default now() not null,
-    updated_at timestamptz default now() not null,
-    deleted_at timestamptz default '0001-01-01 00:00:00Z' not null
-)
+    created_at timestamptz default now(),
+    updated_at timestamptz default now(),
+    deleted_at timestamptz default '0001-01-01 00:00:00Z'
+);
+
+SET timezone to 'UTC'
