8aacfb3e3b72476f00559d55bbd571574b7d18d9
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-08 15:08:12 +0330

    removed not null constraint on timestampz fields

--- db/migration/000002_add_users.up.sql
+++ db/migration/000002_add_users.up.sql
@@ -3,9 +3,9 @@ create table users (
     hashed_password varchar not null,
     fullname varchar(64) not null,
     email varchar unique not null,
-    created_at timestamptz default now() not null,
-    updated_at timestamptz default now() not null,
-    deleted_at timestamptz default '0001-01-01 00:00:00Z' not null
+    created_at timestamptz default now(),
+    updated_at timestamptz default now(),
+    deleted_at timestamptz default '0001-01-01 00:00:00Z'
 );
 
 alter table "accounts" add constraint accounts_owner_fk
