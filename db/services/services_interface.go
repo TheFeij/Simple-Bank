1afdf2f39041aea0b6595ecb7a3f72c389ee8bfb
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-09 14:30:08 +0330

    Updated Services interface

--- db/services/services_interface.go
+++ db/services/services_interface.go
@@ -3,19 +3,18 @@ package services
 import (
 	"Simple-Bank/db/models"
 	"Simple-Bank/requests"
-	"Simple-Bank/responses"
 )
 
 type Services interface {
-	CreateAccount(req requests.CreateAccountRequest) (responses.CreateAccountResponse, error)
-	DeleteAccount(id int64) (responses.GetAccountResponse, error)
-	DepositMoney(req requests.DepositRequest) (responses.EntryResponse, error)
-	WithdrawMoney(req requests.WithdrawRequest) (responses.EntryResponse, error)
-	Transfer(req requests.TransferRequest) (responses.TransferResponse, error)
-	ListAccounts(pageNumber int64, pageSize int8) (responses.ListAccountsResponse, error)
-	GetAccount(id int64) (responses.GetAccountResponse, error)
-	GetTransfer(id int64) (responses.TransferResponse, error)
-	GetEntry(id int64) (responses.EntryResponse, error)
+	CreateAccount(req requests.CreateAccountRequest) (models.Account, error)
+	DeleteAccount(id int64) (models.Account, error)
+	DepositMoney(req requests.DepositRequest) (models.Entry, error)
+	WithdrawMoney(req requests.WithdrawRequest) (models.Entry, error)
+	Transfer(req requests.TransferRequest) (models.Transfer, error)
+	ListAccounts(pageNumber int64, pageSize int8) ([]models.Account, error)
+	GetAccount(id int64) (models.Account, error)
+	GetTransfer(id int64) (models.Transfer, error)
+	GetEntry(id int64) (models.Entry, error)
 	GetUser(username string) (models.User, error)
 	CreateUser(req requests.CreateUserRequest) (models.User, error)
 }
