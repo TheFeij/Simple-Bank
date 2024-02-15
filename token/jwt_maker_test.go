448c4d7d9f5221a1cda17eaaab1b91fc0e5831e8
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-15 22:39:15 +0330

    tested the happy case for JWTMAker

---
+++ token/jwt_maker_test.go
@@ -0,0 +1,32 @@
+package token
+
+import (
+	"Simple-Bank/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestJWTMaker(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomString(32, util.ALL))
+	require.NoError(t, err)
+	require.NotEmpty(t, maker)
+
+	username := util.RandomUsername()
+	duration := 1 * time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.Equal(t, username, payload.Username)
+	require.NotZero(t, payload.ID)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
