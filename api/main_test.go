934f3382c2d58278e8c6d5380bf013382324442d
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-03-02 00:53:37 +0330

    created a NewTestServer function

--- api/main_test.go
+++ api/main_test.go
@@ -1,10 +1,30 @@
 package api
 
 import (
+	"Simple-Bank/config"
+	"Simple-Bank/db/services"
+	"Simple-Bank/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
+	"time"
 )
+
+func NewTestServer(t *testing.T, services services.Services) *Server {
+	config := &config.Config{
+		Token: config.Token{
+			AccessTokenDuration: time.Minute,
+			SymmetricKey:        util.RandomString(32, util.ALL),
+		},
+	}
+
+	server, err := NewServer(config, services)
+	require.NoError(t, err)
+	require.NotEmpty(t, server)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
