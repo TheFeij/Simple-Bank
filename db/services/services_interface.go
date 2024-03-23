671f2998b33c8c524e4fb8b37dcd195133bfc889
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-03-23 21:50:03 +0330

    updated ListAccounts method to only list accounts of the owner

--- db/services/services_interface.go
+++ db/services/services_interface.go
@@ -11,7 +11,7 @@ type Services interface {
 	DepositMoney(req requests.DepositRequest) (models.Entry, error)
 	WithdrawMoney(req requests.WithdrawRequest) (models.Entry, error)
 	Transfer(req requests.TransferRequest) (models.Transfer, error)
-	ListAccounts(pageNumber int64, pageSize int8) ([]models.Account, error)
+	ListAccounts(owner string, pageNumber int64, pageSize int8) ([]models.Account, error)
 	GetAccount(id int64) (models.Account, error)
 	GetTransfer(id int64) (models.Transfer, error)
 	GetEntry(id int64) (models.Entry, error)
