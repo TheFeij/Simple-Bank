bfbbd2cff0f6a3557e731ab47cb8afd72ba2a83a
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-04-25 11:49:12 +0330

    added deleted_at column

--- db/migration/000003_add_sessions.up.sql
+++ db/migration/000003_add_sessions.up.sql
@@ -6,6 +6,7 @@ create table sessions (
     client_ip varchar unique not null,
     is_blocked bool not null default false,
     created_at timestamptz not null default now(),
+    deleted_at timestamptz default '0001-01-01 00:00:00Z',
     expires_at timestamptz not null ,
     foreign key (username) references users(username)
 );
