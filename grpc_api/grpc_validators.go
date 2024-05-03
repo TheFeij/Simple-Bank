1a45203ce1693c87ae052e3e091f4b17d28ac643
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-03 12:09:06 +0330

    defined a function to validate create user rpc request

---
+++ grpc_api/grpc_validators.go
@@ -0,0 +1,24 @@
+package grpc_api
+
+import (
+	"Simple-Bank/pb"
+	"Simple-Bank/util"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+)
+
+func ValidateCreateUserRequest(req *pb.CreateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := util.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+	if err := util.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+	if err := util.ValidateFullname(req.GetFullname()); err != nil {
+		violations = append(violations, fieldViolation("fullname", err))
+	}
+	if err := util.ValidateEmail(req.GetEmail()); err != nil {
+		violations = append(violations, fieldViolation("email", err))
+	}
+
+	return violations
+}
