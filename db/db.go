a552c33f72c9aa20529d0f1b7ae2562a50808775
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-04 21:24:31 +0330

    changed Init function to return an error object

--- db/db.go
+++ db/db.go
@@ -1,20 +1,22 @@
 package db
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
 )
 
 var DB *gorm.DB
 
-func Init(source string) {
+func Init(source string) error {
 	db, err := gorm.Open(postgres.Open(source), &gorm.Config{})
 	if err != nil {
-		log.Fatalln(err)
+		return fmt.Errorf("cannot initialize dataabse: %w", err)
 	}
 
 	DB = db
+
+	return nil
 }
 
 func GetDB() *gorm.DB {
