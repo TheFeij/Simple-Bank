073863ae200df38b2ceee79ab44cf4b90ba8cc17
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-04 10:37:02 +0330

    added a function to validate UpdateUser RPC request

--- grpc_api/grpc_validators.go
+++ grpc_api/grpc_validators.go
@@ -33,3 +33,29 @@ func validateLoginUseRequest(req *pb.LoginUserRequest) (violations []*errdetails
 
 	return violations
 }
+
+func validateUpdateUserRequest(req *pb.UpdateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := util.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if req.Password != nil {
+		if err := util.ValidatePassword(req.GetPassword()); err != nil {
+			violations = append(violations, fieldViolation("password", err))
+		}
+	}
+
+	if req.Fullname != nil {
+		if err := util.ValidateFullname(req.GetFullname()); err != nil {
+			violations = append(violations, fieldViolation("fullname", err))
+		}
+	}
+
+	if req.Email != nil {
+		if err := util.ValidateEmail(req.GetEmail()); err != nil {
+			violations = append(violations, fieldViolation("email", err))
+		}
+	}
+
+	return violations
+}
