d26b13e0bc243d90a0caece64114a229b463a30f
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-04 17:59:21 +0330

    added a GrpcLogger function that uses zerolog package to log necessary information about requests coming to the grpc server

---
+++ grpc_api/logger.go
@@ -0,0 +1,42 @@
+package grpc_api
+
+import (
+	"context"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"time"
+)
+
+func GrpcLogger(
+	ctx context.Context,
+	req any,
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (resp any, err error) {
+
+	start := time.Now()
+	resp, err = handler(ctx, req)
+	duration := time.Since(start)
+
+	statusCode := codes.Unknown
+	if st, ok := status.FromError(err); ok {
+		statusCode = st.Code()
+	}
+
+	logger := log.Info()
+	if err != nil {
+		logger = log.Error().Err(err)
+	}
+
+	logger.
+		Str("protocol", "grpc").
+		Str("method", info.FullMethod).
+		Dur("duration", duration).
+		Int("status_code", int(statusCode)).
+		Str("status_text", statusCode.String()).
+		Msg("received a gRPC request")
+
+	return resp, err
+}
