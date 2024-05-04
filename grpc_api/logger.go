9553ddcd625c984c0a768afa43bb54ebea7cb29a
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-04 12:40:51 +0330

    added a grpc interceptor for logging purposes using zerolog package

--- grpc_api/logger.go
+++ grpc_api/logger.go
@@ -6,7 +6,6 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"net/http"
 	"time"
 )
 
@@ -40,49 +39,4 @@ func GrpcLogger(
 		Msg("received a gRPC request")
 
 	return resp, err
-}
-
-type ResponseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-	Body       []byte
-}
-
-func (recorder *ResponseRecorder) WriteHeader(statusCode int) {
-	recorder.statusCode = statusCode
-	recorder.ResponseWriter.WriteHeader(statusCode)
-}
-
-func (recorder *ResponseRecorder) Write(body []byte) (int, error) {
-	recorder.Body = body
-	return recorder.ResponseWriter.Write(body)
-}
-
-func HttpLogger(
-	handler http.Handler,
-) http.Handler {
-	return http.HandlerFunc(func(response http.ResponseWriter, req *http.Request) {
-		recorder := &ResponseRecorder{
-			ResponseWriter: response,
-			statusCode:     http.StatusOK,
-		}
-
-		start := time.Now()
-		handler.ServeHTTP(recorder, req)
-		duration := time.Since(start)
-
-		logger := log.Info()
-		if recorder.statusCode != http.StatusOK {
-			logger = log.Error().Bytes("body", recorder.Body)
-		}
-
-		logger.
-			Str("protocol", "http").
-			Str("method", req.Method).
-			Str("path", req.RequestURI).
-			Dur("duration", duration).
-			Int("status_code", recorder.statusCode).
-			Str("status_text", http.StatusText(recorder.statusCode)).
-			Msg("received an HTTP request")
-	})
 }
