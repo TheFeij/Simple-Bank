7ae648523e6b80ea152ed86b8f46ffbb0e6d7bd4
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-06-16 18:24:07 +0330

    defined two errors

---
+++ db/services/errors.go
@@ -0,0 +1,8 @@
+package services
+
+import "fmt"
+
+var (
+	ErrUnAuthorizedWithdraw = fmt.Errorf("cannot withdraw money from other user's accounts")
+	ErrUnAuthorizedDeposit  = fmt.Errorf("cannot deposit money into other user's accounts")
+)
