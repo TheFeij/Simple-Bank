bca6459bad7c8fcb7c7a36b815c47149693f346f
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-06-16 18:35:19 +0330

    added ErrUserNotFound and ErrAccountNotFound

--- db/services/errors.go
+++ db/services/errors.go
@@ -5,4 +5,6 @@ import "fmt"
 var (
 	ErrUnAuthorizedWithdraw = fmt.Errorf("cannot withdraw money from other user's accounts")
 	ErrUnAuthorizedDeposit  = fmt.Errorf("cannot deposit money into other user's accounts")
+	ErrUserNotFound         = fmt.Errorf("user not found")
+	ErrAccountNotFound      = fmt.Errorf("account not found")
 )
