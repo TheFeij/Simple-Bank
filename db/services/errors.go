34702d12b69e11451e262ac1f80867d03211e0c8
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-06-16 20:19:30 +0330

    added ErrUnAuthorizedTransfer

--- db/services/errors.go
+++ db/services/errors.go
@@ -5,6 +5,7 @@ import "fmt"
 var (
 	ErrUnAuthorizedWithdraw = fmt.Errorf("cannot withdraw money from other user's accounts")
 	ErrUnAuthorizedDeposit  = fmt.Errorf("cannot deposit money into other user's accounts")
+	ErrUnAuthorizedTransfer = fmt.Errorf("cannot transfer money from other user's accounts")
 	ErrUserNotFound         = fmt.Errorf("user not found")
 	ErrAccountNotFound      = fmt.Errorf("account not found")
 	ErrNotEnoughMoney       = fmt.Errorf("not enough money in the account")
